d9dc66d07cd09615fa01778c9d84c217ec806d75
Leandro Sebastian Rodriguez <lean1998@hotmail.com>
2019-05-17 21:51:50 -0300

    UpdatedMeshImporter

--- ProgramacionDeGraficos2_Clase1/EngineDll/MeshImporter.cpp
+++ ProgramacionDeGraficos2_Clase1/EngineDll/MeshImporter.cpp
@@ -1,5 +1,5 @@
 #include "MeshImporter.h"
-
+// http://ogldev.atspace.co.uk/www/tutorial22/tutorial22.html
 MeshImporter * MeshImporter::instance = NULL;
 
 MeshImporter::MeshImporter()
@@ -17,24 +17,77 @@ void MeshImporter::Clear(vector<BMPData*>& textures)
 {
 	for (int i = 0; i < textures.size(); i++)
 	{
-		delete textures[i];
-		textures[i] = NULL;
+		if (textures[i] != NULL)
+		{
+			delete textures[i];
+			textures[i] = NULL;
+		}
 	}
 }
 
-float * MeshImporter::GetMeshVertex(string path) 
+
+bool MeshImporter::LoadMesh(const string& Filename)
 {
-	float * v;
-	Assimp::Importer importer;
-	const aiScene *scene = importer.ReadFile(path, aiProcess_Triangulate | aiProcess_FlipUVs);
-	if (!scene || scene->mFlags & AI_SCENE_FLAGS_INCOMPLETE || !scene->mRootNode)
-	{
-		cout << "ERROR::ASSIMP::" << importer.GetErrorString() << endl;
-		return;
+	// Release the previously loaded mesh (if it exists)
+	Clear(m_Textures);
+
+	bool Ret = false;
+	Assimp::Importer Importer;
+
+	const aiScene* pScene = Importer.ReadFile(Filename.c_str(), aiProcess_Triangulate | aiProcess_GenSmoothNormals | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
+
+	if (pScene) {
+		Ret = InitFromScene(pScene, Filename);
+	}
+	else {
+		printf("Error parsing '%s': '%s'\n", Filename.c_str(), Importer.GetErrorString());
 	}
 
-	aiVector3D * aivec = scene->mMeshes[0]->mVertices;
-	ai_real x = aivec->x;
-	//x.   
+	return Ret;
 }
+
+bool MeshImporter::InitFromScene(const aiScene* pScene, const string& Filename)
+{
+	m_Entries.resize(pScene->mNumMeshes);
+	m_Textures.resize(pScene->mNumMaterials);
+
+	// Initialize the meshes in the scene one by one
+	for (unsigned int i = 0; i < m_Entries.size(); i++) {
+		const aiMesh* paiMesh = pScene->mMeshes[i];
+		InitMesh(i, paiMesh);
+	}
+	return true;
+}
+
+void MeshImporter::InitMesh(unsigned int Index, const aiMesh* paiMesh)
+{
+
+	vector<Vertex> Vertices;
+	vector<unsigned int> Indices;
+
+	const aiVector3D Zero3D(0.0f, 0.0f, 0.0f);
+
+	for (unsigned int i = 0; i < paiMesh->mNumVertices; i++) {
+		const aiVector3D* pPos = &(paiMesh->mVertices[i]);
+		const aiVector3D* pNormal = &(paiMesh->mNormals[i]);
+		const aiVector3D* pTexCoord = paiMesh->HasTextureCoords(0) ? &(paiMesh->mTextureCoords[0][i]) : &Zero3D;
+
+		Vertex v(vec3(pPos->x, pPos->y, pPos->z),
+			vec2(pTexCoord->x, pTexCoord->y),
+			vec3(pNormal->x, pNormal->y, pNormal->z));
+
+		Vertices.push_back(v);
+	}
+
+	for (unsigned int i = 0; i < paiMesh->mNumFaces; i++) {
+		const aiFace& Face = paiMesh->mFaces[i];
+		assert(Face.mNumIndices == 3);
+		Indices.push_back(Face.mIndices[0]);
+		Indices.push_back(Face.mIndices[1]);
+		Indices.push_back(Face.mIndices[2]);
+	}
+
+	m_Entries[Index].Init(Vertices, Indices);
+}
+
 
--- ProgramacionDeGraficos2_Clase1/EngineDll/MeshImporter.h
+++ ProgramacionDeGraficos2_Clase1/EngineDll/MeshImporter.h
@@ -9,7 +9,12 @@
 #include "TextureImporter.h"
 #include <vector>
 #include <iostream>
+#include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
+#include <gl/glew.h>
 using namespace std;
+using namespace glm;
+
 class ENGINEDLL_API MeshImporter
 {
 public:
@@ -22,7 +27,41 @@ public:
 			instance = new MeshImporter();
 		return instance;
 	}
+	bool LoadMesh(const string& Filename);
 	void Clear(vector<BMPData*>& textures);
-	float * GetMeshVertex(string path);
+	bool InitFromScene(const aiScene* pScene, const std::string& Filename);
+	void InitMesh(unsigned int Index, const aiMesh* paiMesh);
+
+	struct Vertex
+	{
+		vec3 m_pos;
+		vec2 m_tex;
+		vec3 m_normal;
+
+		Vertex() {}
+
+		Vertex(const vec3& pos, const vec2& tex, const vec3& normal)
+		{
+			m_pos = pos;
+			m_tex = tex;
+			m_normal = normal;
+		}
+	};
+
+	struct MeshEntry {
+		MeshEntry();
+
+		~MeshEntry();
+
+		void Init(const vector<Vertex>& Vertices,
+			const vector<unsigned int>& Indices);
+			
+		GLuint VB;
+		GLuint IB;
+		unsigned int NumIndices;
+	};
+
+	vector<MeshEntry> m_Entries;
+	vector<BMPData*> m_Textures;
 };
 
